95d06a1541e0a1897b4fe4517d40bcad85d45165
letonio <letoniojs@gmail.com>
2022-11-09 08:25:14 -0300

    upd: v2.1 Botoes visiveis e invisiveis

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/*
-Creio que na verdade, a gente vai precisar de uma variavel que tem o endereco
-do container da tabela, a gente adiciona um event listener no container,
-quando o usuario clicar em um elemento dessa tabela
-a gente verifica se o elemento em questao é a imagem de editar ou a imagem
-de apagar, essa imagem tem que ter um id, de preferencia associado ao indice
-do elemento em questão, então sabendo o id dessa imagem
-chamar uma fucnao que apague o elemento ou edite o elemento,
-reenumere o indice dos elementos restantes, e mostre na tela a tabela atualziada
-
-*/
-/*
-document.querySelector("container").addEventListener("click", (event) => {
-	console.log(event.target.parentNode.childNodes[1].textContent);
-});
-*/
-
 console.log("Start!");
 
 //VARIAVEIS e CONSTANTES GLOBAIS - DESCRICAO:
@@ -27,23 +10,14 @@ let countId = 0;
 //contador que corresponde ao id
 //sempre incrementa de 1 em 1, garantindo a unicidade
 //de cada identificador
-const botaoIncluirProduto = document.querySelector("#incluirBotao");
-const botaoListarProdutos = document.querySelector("#listarBotao");
-
 const inputNome = document.querySelector("#inputNomeProduto");
 const inputDescricao = document.querySelector("#inputDescricaoProduto");
 const inputValor = document.querySelector("#inputValorProduto");
-
+const botaoIncluirProduto = document.querySelector("#incluirBotao");
+const botaoListarProdutos = document.querySelector("#listarBotao");
 const botaoConfirmaEdicao = document.querySelector("#atualizarBotao"); //
-//esse botao comeca invisivel e so fica visivel se o usuario clicou na imagem da tabela que
-//faz referencia a edicao
 const botaoCancelaEdicao = document.querySelector("#cancelarBotao");
-//esse botao comeca invisivel e so fica visivel se o usuario clicou na imagem da tabela que
-//faz referencia a edicao
-
 const containerTabela = document.querySelector(".lista-produtos");
-// se dentro do elemento de tabela o usuario clicar na imagem de editar ou na imagem de apagar
-// deve mostrar
 const dadosDoProdutoClicado = document.querySelector("#produto-clicado");
 const mensagemErro = document.querySelector("#mensagem-erro");
 let produtoApagado = {};
@@ -52,7 +26,7 @@ let produtoEditado = {};
 //armazena as informações do produto que será/foi editado
 
 //FUNCÕES
-//colocar try catch
+
 botaoIncluirProduto.addEventListener("click", () => {
 	try {
 		produtoEhValido();
@@ -79,9 +53,8 @@ function produtoEhValido() {
 	}
 }
 
-//adicionar produto no array
-//pode ter dentro dela varias funcoes que adiciona cada elemento
 function adicionaProdutoNoArray() {
+	dadosDoProdutoClicado.innerHTML = ""; //
 	let produto = {};
 	countId = countId + 1;
 	//{id:"",nome:"",descricao:"",valor:"","incluidoEm"}
@@ -102,8 +75,6 @@ botaoListarProdutos.addEventListener("click", () => {
 	mostraProdutos();
 });
 
-// se o usuario clicou no botao que e´o mesmo para adicionar produto e editar
-// verifica se esta na situação de editar, faz a alteracao e mostra a tabela atualizada
 botaoConfirmaEdicao.addEventListener("click", () => {
 	editaProduto();
 	escondeBotaoComId(botaoConfirmaEdicao);
@@ -121,25 +92,21 @@ botaoCancelaEdicao.addEventListener("click", () => {
 	//mostraProdutos();
 });
 
-/*NOVA ABORDAGEM CONTAINER TABELA!!!!!!!!!!!!*/
-
 containerTabela.addEventListener("click", (event) => {
 	const element = event.target.parentNode;
 
 	if (element.classList.contains("editar-produto")) {
-		console.log("clicou no botao editar!!!");
 		//console.log(element.parentNode);
 		const idElement = parseInt(element.parentNode.childNodes[1].textContent);
 		indiceElement = achaIndiceDoProduto(idElement);
 		produtoEditado = listaDeProdutos[indiceElement];
 		mostraDadosDoProduto(produtoEditado);
 		//console.log(element.parentNode.childNodes[1].textContent);
-		//escondeBotaoComId(botaoIncluirProduto);
-		//escondeBotaoComId(botaoListarProdutos);
-		//mostraBotaoComId(botaoConfirmaEdicao);
-		//mostraBotaoComId(botaoCancelaEdicao);
+		escondeBotaoComId(botaoIncluirProduto);
+		escondeBotaoComId(botaoListarProdutos);
+		mostraBotaoComId(botaoConfirmaEdicao);
+		mostraBotaoComId(botaoCancelaEdicao);
 	} else if (element.classList.contains("apagar-produto")) {
-		console.log("clicou no botao apagar!!!");
 		//console.log(element.parentNode);
 		//console.log(element.parentNode.childNodes[1].textContent);
 		const idElement = parseInt(element.parentNode.childNodes[1].textContent);
@@ -152,28 +119,12 @@ containerTabela.addEventListener("click", (event) => {
 		indiceElement = achaIndiceDoProduto(idElement);
 		mostraProdutoSelecionado(indiceElement);
 	}
-	/*
-	if (event.target == "ImagemDeEditar") {
-		//pegue ID ou class desse target
-		// edite o elemento
-		//armazena na variavel global produtoEditado;
-		armazenaTargetNaVariavel(event.target, produtoEditado);
-		escondeBotaoComId(botaoIncluirProduto);
-		escondeBotaoComId(botaoListarProdutos);
-		mostraBotaoComId(botaoConfirmaEdicao);
-		mostraBotaoComId(botaoCancelaEdicao);
-	} else if (event.target == "ImagemDeApagar") {
-		//pegue ID ou class desse target
-		// apague o elemento
-		//armazena na variavel global produtoApagado;
-		armazenaTargetNaVariavel(event.target, produtoApagado);
-		apagaProduto();
-	}*/
 });
 
 const tabelaDeProdutos = document.querySelector(".tabela-produtos");
 
 function mostraProdutos() {
+	dadosDoProdutoClicado.innerHTML = ""; //
 	tabelaDeProdutos.innerHTML = `
 	<tr class="linha-tabela table-heading">
 						<td class="id-produto">ID</td>
@@ -190,7 +141,9 @@ function mostraProdutos() {
 		tr.innerHTML = `
 						<td class="id-produto">${listaDeProdutos[indice].id}</td>
 						<td class="nome-produto">${listaDeProdutos[indice].nome}</td>
-						<td class="valor-produto">${listaDeProdutos[indice].valor}</td>
+						<td class="valor-produto">R$ ${padronizaValorParaReais(
+							listaDeProdutos[indice].valor
+						)}</td>
 						<td class="editar-produto">
 							<img src="./assets/edit.png" alt="Ícone de editar produto" />
 						</td>
@@ -207,7 +160,8 @@ function mostraProdutos() {
 	console.log("ULTIMA LINHA DA TABELA:", tabelaDeProdutos);
 	//create a line for each product
 }
-function armazenaTargetNaVariavel(target, produto) {}
+//function armazenaTargetNaVariavel(target, produto) {}
+
 function mostraDadosDoProduto(produto) {
 	console.log("O produto que precisa aparecer na tela é:", produto);
 	inputNome.value = `${produto.nome}`;
@@ -221,9 +175,17 @@ function editaProduto() {
 	listaDeProdutos[indiceElement].valor = inputValor.value;
 	const currentDate = new Date();
 	listaDeProdutos[indiceElement].incluidoEm = currentDate.toISOString();
+
+	mensagemErro.innerHTML = `Produto ${listaDeProdutos[indiceElement].nome} atualizado com sucesso!`;
+	mensagemErro.style.visibility = "visible";
+	mensagemErro.style.color = "green";
 }
-function escondeBotaoComId(botaoId) {}
-function mostraBotaoComId(botaoId) {}
+function escondeBotaoComId(botao) {
+	botao.classList.add("display-none");
+}
+function mostraBotaoComId(botao) {
+	botao.classList.remove("display-none");
+}
 
 function achaIndiceDoProduto(idElement) {
 	let indice = 0;
@@ -240,12 +202,7 @@ function achaIndiceDoProduto(idElement) {
 function apagaProduto(idElement) {
 	let indice = 0;
 	while (indice < listaDeProdutos.length) {
-		console.log(
-			"idElement = ",
-			idElement,
-			"id do objeto=",
-			listaDeProdutos[indice].id
-		);
+		//console.log("dElement = ",idElement,"id do objeto=",listaDeProdutos[indice].id);
 		if (idElement === listaDeProdutos[indice].id) {
 			console.log("Precisa apagar o elemento", listaDeProdutos[indice]);
 			produtoApagado = listaDeProdutos[indice];
@@ -265,10 +222,20 @@ function mostraProdutoSelecionado(indiceElement) {
 	<p>Id: ${listaDeProdutos[indiceElement].id}</p>
 	<h3>Nome: ${listaDeProdutos[indiceElement].nome}</h3>
 	<p>Descrição: ${listaDeProdutos[indiceElement].descricao}</p>
-	<h4>Valor: R$ ${listaDeProdutos[indiceElement].valor}</h4>
+	<h4>Valor: R$ ${padronizaValorParaReais(
+		listaDeProdutos[indiceElement].valor
+	)}</h4>
 	<p>Incluído em: ${stringData}</p>
 	`;
 }
+
+function padronizaValorParaReais(valor) {
+	console.log("esse é o valor", valor);
+	const real = parseFloat(valor).toFixed(2);
+	console.log("esse é o real", real);
+	return real;
+}
+
 function passaDataParaFormatoPadrao(data) {
 	console.log("data:", data);
 	console.log("dia:", data.substr(8, 2));
